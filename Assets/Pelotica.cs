6d4135eca25b17aafd8025cf95f56e6785c32f1b
SalchichamanIII <franhp10b@gmail.com>
2024-10-14 00:53:46 +0200

    141024

--- Assets/Pelotica.cs
+++ Assets/Pelotica.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Pelotica : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float fuerza;
+    [SerializeField] float distanciaChoqueSuelo;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        saltoNormal();
+        estaEnElSuelo();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        condicionSalto();
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector3(movimientoHorizontal, 0.0f, movimientoVertical);
+        rb.AddForce(movimiento * velocidad);
+    }
+
+    private void condicionSalto()
+    {
+        if(Input.GetKey(KeyCode.Space) && estaEnElSuelo())
+        {
+            saltoNormal();
+        }
+            
+                       
+                
+    }
+
+    private void saltoNormal()
+    {
+        rb.AddForce (Vector3.up * fuerza , ForceMode.Impulse);
+    }
         
+
+   
+
+    private bool estaEnElSuelo()
+    {
+        return Physics.Raycast(Transform.position, Vector3.down , distanciaChoqueSuelo);
     }
 }
