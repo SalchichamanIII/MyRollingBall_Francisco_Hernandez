11661289ada627f14876f7303798eb8e49ece14c
SalchichamanIII <163415869+SalchichamanIII@users.noreply.github.com>
2024-10-14 21:19:11 +0200

    141024

---
+++ Assets/Scripts/rotacionCol.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotacionCol : MonoBehaviour
+{
+    float velocidad = 18;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,0,10)* velocidad * Time.deltaTime);
+    }
+}
