539d1243e820137616163b8484d16e3856bacec3
SalchichamanIII <163415869+SalchichamanIII@users.noreply.github.com>
2024-11-04 00:36:14 +0100

    4444545

---
+++ Assets/Scripts/Coleccionable.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coleccionable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hola"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
