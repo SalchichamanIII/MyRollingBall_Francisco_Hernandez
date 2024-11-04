e7ef73a3173faa31c2a8693c4dd7ec88b6e36bd5
SalchichamanIII <163415869+SalchichamanIII@users.noreply.github.com>
2024-11-04 18:24:04 +0100

    041124

--- Assets/Scripts/Inventario.cs
+++ Assets/Scripts/Inventario.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Inventario : MonoBehaviour
 {
 
     public int cantidad = 0;
+    public TextMeshProUGUI numero;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,6 @@ public class Inventario : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        numero.text = cantidad.ToString();
     }
 }
